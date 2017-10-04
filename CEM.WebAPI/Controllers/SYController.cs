e8953c039da21c25097836a60c34494f784b3241
Michael Amadore <MICHAELA@magenic.com>
2017-10-04 15:39:49 +0800

    added sy controller

---
+++ CEM.WebAPI/Controllers/SYController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CEM.WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SY")]
+    public class SYController : Controller
+    {
+
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+    }
+}
